106e1a6efd46b519443fc368961b46c4c6708f50
Alfredo Junior <jrcdz01@gmail.com>
2023-02-05 18:11:23 -0300

    Add files via upload

---
+++ src/spritesJogo/tilemapTowerDefenser.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="spriteTile" tilewidth="64" tileheight="64" tilecount="54" columns="9">
+ <image source="tileMap/spriteTile.png" width="600" height="420"/>
+</tileset>
